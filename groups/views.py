36855c48afe8600f8ab7291d02b7d15e223c9ea0
mdronski <dronskimichal@gmail.com>
2020-09-05 22:27:26 +0200

    Fill group templates

--- groups/views.py
+++ groups/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import CreateView, DetailView, ListView
 
-# Create your views here.
+from groups.models import Group
+
+
+class CreateGroupView(LoginRequiredMixin, CreateView):
+    model = Group
+    fields = ['name', 'description']
+
+
+class SingleGroup(DetailView):
+    model = Group
+
+
+class ListGroups(ListView):
+    model = Group

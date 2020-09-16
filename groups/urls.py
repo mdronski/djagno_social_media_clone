eec2b02142130e967322185e275c83646c2e514e
mdronski <dronskimichal@gmail.com>
2020-09-16 22:09:29 +0200

    Create join leave group views

--- groups/urls.py
+++ groups/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('', views.ListGroups.as_view(), name='all'),
     path('new/', views.CreateView, name='create'),
     path('posts/in/<slug:slug>/', views.SingleGroup.as_view(), name='single'),
+    path('join/<slug:slug>/', views.JoinGroup.as_view(), name='join'),
+    path('leave/<slug:slug>/', views.LeaveGroup.as_view(), name='delete'),
 ]
